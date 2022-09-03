9a40bff67943665fe523c55dbbfee2e9a767018d
Leonel Meque <leomeque@gmail.com>
2022-09-03 21:39:50 +0200

    feat(todo-mobile): add Layout component

---
+++ packages/todo-mobile/src/components/atoms/Layout/Layout.tsx
@@ -0,0 +1,5 @@
+import { StyledLayout } from "./styles";
+
+const Layout = ({ children }: { children: any }) => <StyledLayout>{children}</StyledLayout>
+
+export default Layout;
---
+++ packages/todo-mobile/src/components/atoms/Layout/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+import { View } from 'react-native';
+
+export const StyledLayout = styled(View)`
+  padding-horizontal: 16px;
+  
+`
