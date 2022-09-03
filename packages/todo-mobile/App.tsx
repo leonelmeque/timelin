a608b7107eeda0b2d9993fc5129dd651def993fd
Leonel Meque <leomeque@gmail.com>
2022-09-04 01:50:34 +0200

    chore(todo-mobile): general cleaning

--- packages/todo-mobile/App.tsx
+++ packages/todo-mobile/App.tsx
@@ -1,26 +1,15 @@
-import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView, View } from 'react-native'
-import Button from './src/components/atoms/Button/Button'
 import { ThemeProvider } from 'styled-components/native'
 import { lightTheme, darkTheme } from './src/utils/theme'
 import { useThemeSwitcher } from './src/hooks/use-theme-switcher'
 import { ThemeContext } from './src/context'
-import Layout from './src/components/atoms/Layout/Layout'
+import TodosScreen from './src/screens/todos-screen'
 
 export default function App() {
   const [activeTheme, themeToggler] = useThemeSwitcher()
   return (
     <ThemeProvider theme={activeTheme === 'light' ? lightTheme : darkTheme}>
       <ThemeContext>
-        <SafeAreaView>
-          <StatusBar style="auto" />
-          <Layout>
-            <Button label='React native is great' variant='secondary' size='md' />
-            <Button label='Switch Theme' variant='primary' size='lg' onPress={() => {
-              themeToggler()
-            }} />
-          </Layout>
-        </SafeAreaView>
+        <TodosScreen />
       </ThemeContext>
     </ThemeProvider>
   )
---
+++ packages/todo-mobile/src/components/index.ts
@@ -0,0 +1,4 @@
+import TodoCard from './organisms/TodoCard/todo-card'
+import Button from './atoms/Button/Button'
+
+export { TodoCard, Button }
