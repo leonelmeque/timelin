c066586be659783f6ff02955bb0afbf84776b145
Leonel Meque <leomeque@gmail.com>
2022-09-04 18:56:22 +0200

    feat(todo-mobile): add id generator

--- packages/todo-mobile/src/components/index.ts
+++ packages/todo-mobile/src/components/index.ts
@@ -2,5 +2,6 @@ import TodoCard from './organisms/TodoCard/todo-card'
 import Button from './atoms/Button/Button'
 import Typography from './atoms/Typography/typography'
 import Header from './molecules/Header/header'
+import TextButton from './atoms/TextButton/TextButton'
 
-export { TodoCard, Button, Typography, Header }
+export { TodoCard, Button, Typography, Header, TextButton }
