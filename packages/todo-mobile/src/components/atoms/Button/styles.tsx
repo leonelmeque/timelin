a882cc83da5630f5a7895f5d40cca5068c52b5da
Leonel Meque <leomeque@gmail.com>
2022-09-04 16:12:09 +0200

    feat(todo-mobile): add danger styles to button

--- packages/todo-mobile/src/components/atoms/Button/styles.tsx
+++ packages/todo-mobile/src/components/atoms/Button/styles.tsx
@@ -1,37 +1,41 @@
-import { Text } from "react-native";
-import styled, { css } from "styled-components/native";
+import { Text } from 'react-native'
+import styled, { css } from 'styled-components/native'
 import { typography } from '../../../tokens'
-import { unitsConverter } from "../../../utils/uiUtils";
+import { unitsConverter } from '../../../utils/uiUtils'
 
 export const _variants = {
   primary: css`
-    background: ${props => props.theme.colours.primary};
-    color: ${props => props.theme.colours.white};
+    background: ${(props) => props.theme.colours.primary};
+    color: ${(props) => props.theme.colours.white};
   `,
   secondary: css`
-    background: ${props => props.theme.colours.accent};
-    color: ${props => props.theme.colours.dark};
+    background: ${(props) => props.theme.colours.accent};
+    color: ${(props) => props.theme.colours.dark};
   `,
-  tertiary: "",
-};
+  tertiary: css``,
+  danger: css`
+    background-color: ${(props) => props.theme.colours.danger};
+    color: ${(props) => props.theme.colours.white};
+  `,
+}
 
 export const _sizes = {
   sm: css`
-    font-size: ${props => props.theme.typography.sizes.small}px;
-    padding: ${props => props.theme.sizes.small}px;
-    border-radius: ${props => props.theme.sizes.small / 2}px;
+    font-size: ${(props) => props.theme.typography.sizes.small}px;
+    padding: ${(props) => props.theme.sizes.small}px;
+    border-radius: ${(props) => props.theme.sizes.small / 2}px;
   `,
   md: css`
-    font-size: ${props => props.theme.typography.sizes.body}px;
-    padding: ${props => props.theme.sizes.medium}px;
-    border-radius: ${props => props.theme.sizes.medium / 2}px;
+    font-size: ${(props) => props.theme.typography.sizes.body}px;
+    padding: ${(props) => props.theme.sizes.medium}px;
+    border-radius: ${(props) => props.theme.sizes.medium / 2}px;
   `,
   lg: css`
     font-size: ${unitsConverter(typography.sizes.large)};
-    padding: ${unitsConverter(16)} ${unitsConverter(18)};
+    padding: ${unitsConverter(24)} ${unitsConverter(16)};
     border-radius: ${unitsConverter(4)};
   `,
-};
+}
 
 export const Label = styled(Text)`
   color: white;
