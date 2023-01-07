f55a2ad0dd87eedde24925d87df3aea39bf42c02
Leonel Meque <leomeque@gmail.com>
2023-01-07 03:32:20 +0100

    chore(config): remove unneeded code

--- scripts/build.ts
+++
@@ -1,6 +0,0 @@
-import { moduleBuilder, wasModuleTypeProvided } from './helpers/utils'
-
-const [type, moduleName] = process.argv.slice(2)
-
-wasModuleTypeProvided(type as never)
-moduleBuilder(type as never, moduleName)
--- scripts/install.ts
+++
@@ -1,6 +0,0 @@
-import { moduleInstaller, wasModuleTypeProvided } from './helpers/utils'
-
-const [type, moduleName] = process.argv.slice(2)
-
-wasModuleTypeProvided(type as never)
-moduleInstaller(type as never, moduleName)
