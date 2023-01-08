d960d21fdb782479c004b10e3ca0c67f6dd5a512
Leonel Meque <leomeque@gmail.com>
2023-01-08 17:20:34 +0100

    feat(commons): add design tokens

---
+++ packages/commons/src/tokens/sizes.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components/native"
+
+const sizes: DefaultTheme['sizes'] = {
+  extraSmall: 8,
+  small: 12,
+  medium: 14,
+  large: 16,
+  extraLarge: 20,
+  big: 24
+}
+
+export { sizes }
