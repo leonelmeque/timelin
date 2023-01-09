00a8eeab5809f1cf28e461af574491aee4a3bbd1
Leonel Meque <leomeque@gmail.com>
2023-01-09 01:42:15 +0100

    fix(web): packages issues

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,28 @@
 import './App.css';
+import { hooks, theme } from '@todo/commons';
+import { ThemeProvider } from 'styled-components';
 
 function App() {
-  return <div className="App" />;
+  const { useGreetings, useThemeSwitcher } = hooks;
+  const [activeTheme, toggleTheme] = useThemeSwitcher();
+  console.log(theme[activeTheme]);
+  const response = useGreetings({ whoToGreet: 'Leonel' });
+  return (
+    <ThemeProvider theme={theme[activeTheme]}>
+      <div className="App">
+        <h2>
+          <span>{response}</span>
+        </h2>
+        <button
+          type="button"
+          onClick={toggleTheme}
+          style={{ background: theme[activeTheme].colours.primary }}
+        >
+          Switch theme
+        </button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
