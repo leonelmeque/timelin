6ffa86b1baa8979d32d43e5f77d60ea10475962e
Leonel Meque <leomeque@gmail.com>
2023-01-14 19:55:31 +0100

    feat(mobile-ui): add button screen

--- packages/mobile-ui/default.tsx
+++ packages/mobile-ui/default.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { hooks, theme } from '@todo/commons';
-import { ThemeProvider } from 'styled-components/native';
-import StorybookUIRoot from './storybook';
+import styled, { ThemeProvider } from 'styled-components/native';
+import { Text, SafeAreaView } from 'react-native';
+import { Button } from './src';
+
+const Container = styled.View`
+  flex: 1;
+  padding: 16px;
+`;
+
+const Spacer = styled.View<{ size?: number }>`
+  padding: ${(props) => (props.size ? props.size * 2 : 4)}px;
+`;
+
+const ButtonsScreen = () => (
+  <Container>
+    <Text>Buttons</Text>
+    <Spacer size={4} />
+    <Button label="Primary large" size="lg" variant="primary" />
+    <Spacer size={8} />
+    <Button label="Button" size="sm" variant="primary" />
+  </Container>
+);
 
 export const Default = () => {
   const { useThemeSwitcher } = hooks;
   const [selectedTheme] = useThemeSwitcher();
   return (
     <ThemeProvider theme={theme[selectedTheme]}>
-      <StorybookUIRoot />
+      <SafeAreaView style={{ flex: 1 }}>
+        <ButtonsScreen />
+      </SafeAreaView>
     </ThemeProvider>
   );
 };
