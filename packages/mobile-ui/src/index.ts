02e42f50a92520e09398e29e28bb0ec7019a1cf5
Leonel Meque <leomeque@gmail.com>
2023-01-16 00:26:19 +0100

    feat(mobile): add palette atom

--- packages/mobile-ui/src/index.ts
+++ packages/mobile-ui/src/index.ts
@@ -3,3 +3,4 @@ export { Button } from './atoms/button';
 export { Text } from './atoms/typography';
 export { Spacer } from './atoms/spacer';
 export { Input } from './atoms/input';
+export { Palette } from './atoms/palette';
