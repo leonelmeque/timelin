a5148cf5e3794f10c83f9cbc440bdb428c93a274
Leonel Meque <leomeque@gmail.com>
2023-01-18 17:28:23 +0100

    fix(mobile): code improvement

--- apps/mobile/src/components/index.ts
+++ apps/mobile/src/components/index.ts
@@ -1,7 +1,6 @@
-import TodoCard from './organisms/TodoCard/todo-card'
-import Button from './atoms/Button/Button'
-import Typography from './atoms/Typography/typography'
-import Header from './molecules/Header/header'
-import TextButton from './atoms/TextButton/TextButton'
+import Button from './atoms/Button/Button';
+import Typography from './atoms/Typography/typography';
+import { Header } from './molecules/Header/header';
+import TextButton from './atoms/TextButton/TextButton';
 
-export { TodoCard, Button, Typography, Header, TextButton }
+export { Button, Typography, Header, TextButton };
