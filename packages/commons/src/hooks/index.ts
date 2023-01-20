fdd00ccd0ad5aacda7e7c46874023d04cbe90513
Leonel Meque <leomeque@gmail.com>
2023-01-20 22:39:08 +0100

    feat(commons): add search todo hook

--- packages/commons/src/hooks/index.ts
+++ packages/commons/src/hooks/index.ts
@@ -1,7 +1,9 @@
 import { useGreetings } from './use-greetings';
 import { useThemeSwitcher } from './use-theme-switcher';
+import { useSearchTodos } from './use-search-todos';
 
 export const hooks = {
   useGreetings,
   useThemeSwitcher,
+  useSearchTodos,
 };
