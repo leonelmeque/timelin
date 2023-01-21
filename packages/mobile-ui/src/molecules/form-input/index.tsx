54d5970821cae62707588c82f1ca551e7bfc28d4
Leonel Meque <leomeque@gmail.com>
2023-01-21 02:01:55 +0100

    feat(mobile-ui): add form input component

---
+++ packages/mobile-ui/src/molecules/form-input/index.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+import { TextInputProps } from 'react-native';
+import { Caption } from '../../atoms/caption';
+import { Input } from '../../atoms/input';
+import { Spacer } from '../../atoms/spacer';
+import { Text } from '../../atoms/typography';
+
+interface FormInputProps extends TextInputProps {
+  label: string;
+  captionText?: string;
+  errorText?: string;
+  successText?: string;
+  variant?: 'error' | 'success' | 'caption';
+}
+
+export const FormInput: FC<FormInputProps> = ({
+  label,
+  captionText,
+  errorText,
+  successText,
+  variant,
+  ...rest
+}) => {
+  const caption =
+    (variant === 'error' && errorText) ||
+    (variant === 'success' && successText) ||
+    captionText ||
+    '';
+
+  return (
+    <>
+      <Text size="body" weight="bold">
+        {label}
+      </Text>
+      <Spacer size="4" />
+      <Input
+        {...rest}
+        hasError={variant === 'error'}
+        success={variant === 'success'}
+      />
+      <Spacer size="4" />
+      {<Caption variant={variant} caption={caption} />}
+    </>
+  );
+};
+
+FormInput.displayName = 'FormInput';
