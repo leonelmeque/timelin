f8c937f0bd8ad1e634dd9d5308fecb2a6c4af6a9
Leonel Meque <leomeque@gmail.com>
2023-01-24 19:53:12 +0100

    feat(mobile-ui): made header more flexible

--- packages/mobile-ui/src/organisms/header/styles.tsx
+++ packages/mobile-ui/src/organisms/header/styles.tsx
@@ -9,27 +9,18 @@ export const Container = styled(Pressable)`
   flex-direction: row;
 `;
 
-export const AddTodoButton = styled(Pressable)`
-  border-radius: ${(props) => props.theme.sizes.big * 9000}px;
+export const HeaderLeftContent = styled.View`
+  order: 1;
+  justify-content: flex-start;
+`;
+
+export const HeaderMiddleContent = styled.View`
+  order: 2;
   justify-content: center;
-  padding: 14px 16px;
-  background-color: ${(props) => props.theme.colours.neutrals.white};
-  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
 `;
 
-export const FunnyBadge = styled.View`
-  width: 229.37px;
-  height: 134.15px;
-  position: absolute;
-  border-radius: 24px;
-  background: ${({ theme: { colours } }) => colours.primary.P300};
-  top: -75px;
-  left: -30px;
-  transform: rotate(-9deg);
+export const HeaderRightContent = styled.View`
+  order: 3;
+  justify-content: flex-end;
 `;
 
-export const Avatar = styled(Av)`
-  width: 48px;
-  height: 48px;
-  border-radius: 8px;
-`;
