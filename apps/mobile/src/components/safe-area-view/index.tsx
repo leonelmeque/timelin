c50ad77498bbe540aed9b7b6a688facfc170ab69
Leonel Meque <leomeque@gmail.com>
2023-01-24 19:59:20 +0100

    fix(mobile): removed funny badge from ui

--- apps/mobile/src/components/safe-area-view/index.tsx
+++ apps/mobile/src/components/safe-area-view/index.tsx
@@ -3,19 +3,8 @@ import { SafeAreaView as SafeArea } from 'react-native';
 import { SafeAreaViewProps } from 'react-native-safe-area-context';
 import { FunnyBadge } from '../molecules/Header/styles';
 
-export const CustomSafeAreaView: FC<SafeAreaViewProps> = ({
-  children,
-  ...rest
-}) => (
-  <SafeArea {...rest}>
-    <FunnyBadge
-      style={{
-        top: -5,
-        zIndex: 0,
-      }}
-    />
-    {children}
-  </SafeArea>
+export const CustomSafeAreaView: FC<SafeAreaViewProps> = ({ children, ...rest }) => (
+  <SafeArea {...rest}>{children}</SafeArea>
 );
 
 CustomSafeAreaView.displayName = 'CustomSafeAreaView';
