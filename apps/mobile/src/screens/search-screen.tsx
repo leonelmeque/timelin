7fe24ed0297b79103fb346aec4dd66c709c8f560
Leonel Meque <leomeque@gmail.com>
2023-01-24 20:00:35 +0100

    feat(mobile): add new header to home and search screen

--- apps/mobile/src/screens/search-screen.tsx
+++ apps/mobile/src/screens/search-screen.tsx
@@ -1,9 +1,10 @@
 import { CustomSafeAreaView } from '../components/safe-area-view';
 import { TodoProps } from '@todo/commons';
-import { Header, Spacer } from '@todo/mobile-ui';
+import { Header, Spacer, Text } from '@todo/mobile-ui';
 import { FC } from 'react';
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { SearchView } from '../components/search-view';
+import { Pressable } from 'react-native';
 
 type SearchScreenProps = {
   Params: {
@@ -15,10 +16,18 @@ export const SearchScreen: FC = () => {
   const {
     params: { todos },
   } = useRoute<RouteProp<SearchScreenProps>>();
+  const navigation = useNavigation();
 
   return (
     <CustomSafeAreaView style={{ flex: 1 }}>
-      <Header />
+      <Header
+        renderLeftContent={() => (
+          <Pressable onPress={() => navigation.goBack()}>
+            <Text size="body">Back</Text>
+          </Pressable>
+        )}
+        renderRigthContent={() => <Text size="body">Search</Text>}
+      />
       <Spacer size="8" />
       <SearchView data={todos} />
     </CustomSafeAreaView>
