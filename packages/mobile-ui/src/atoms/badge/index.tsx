e9871e1c26ce02967f4a5c1c06008f6e4dc991d2
Leonel Meque <leomeque@gmail.com>
2023-01-24 21:51:39 +0100

    feat(mobile-ui): improved badge substring function

--- packages/mobile-ui/src/atoms/badge/index.tsx
+++ packages/mobile-ui/src/atoms/badge/index.tsx
@@ -1,13 +1,17 @@
 import { FC } from 'react';
-import { BadgeBGColor, BadgeTextColor } from '../../shared-types';
+import { BadgeBGColor } from '../../shared-types';
 import { Text } from '../typography';
 import { StyledBadge } from './styles';
 
-export const Badge: FC<{ status: keyof typeof BadgeBGColor }> = ({ status }) => (
+export const Badge: FC<{ status: keyof typeof BadgeBGColor }> = ({
+  status,
+}) => (
   <StyledBadge bg={BadgeBGColor[status]}>
     <Text size="small" weight="medium">
-      {status.substring(0, 1).toUpperCase() +
-        status.substring(1, status.length).toLocaleLowerCase()}
+      {(
+        status.substring(0, 1).toUpperCase() +
+        status.substring(1, status.length).toLocaleLowerCase()
+      ).replace(/_/g, ' ')}
     </Text>
   </StyledBadge>
 );
