ecca74c9b7bdf4e10793f000bd72a16c3ee6215e
Leonel Meque <leomeque@gmail.com>
2023-01-24 21:52:45 +0100

    feat(mobile): add list todo screen

--- apps/mobile/src/routes/todo-routes.tsx
+++ apps/mobile/src/routes/todo-routes.tsx
@@ -3,22 +3,23 @@ import {
   NativeStackNavigationOptions,
 } from '@react-navigation/native-stack'
 import AddTodoScreen from '../screens/add-todo-screen'
-import TodosScreen from '../screens/list-todo-screen'
+import HomeScreen from '../screens/home-screen';
+import { ListTodoScreen } from '../screens/list-todo-screen';
 import { SearchScreen } from '../screens/search-screen';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 const options: NativeStackNavigationOptions = {
   headerShown: false,
-}
+};
 
 const TodoListStack = () => (
   <Stack.Navigator screenOptions={options}>
-    <Stack.Screen component={TodosScreen} name="TodoList" />
+    <Stack.Screen component={HomeScreen} name="TodoList" />
     <Stack.Screen
-      component={AddTodoScreen}
-      name="Todo/Main"
-      options={{ headerTitle: 'Add Todo', headerShown: true }}
+      component={ListTodoScreen}
+      name="Todo/ListTodo"
+      options={{ headerShown: false }}
     />
     <Stack.Screen
       component={SearchScreen}
