0addf316408a09d3f444563b134aac59082f09fd
Leonel Meque <leomeque@gmail.com>
2023-01-24 19:52:59 +0100

    feat(mobile): todo card now has avatars

--- packages/mobile-ui/src/organisms/todo-card/index.tsx
+++ packages/mobile-ui/src/organisms/todo-card/index.tsx
@@ -1,16 +1,23 @@
-import { dateFormatter, TodoProps, tokens, UserProps } from '@todo/commons';
-import { FC } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { dateFormatter, hooks, TodoProps, tokens } from '@todo/commons';
+import { FC, useCallback } from 'react';
 import { Avatar } from '../../atoms/avatar';
 import { Badge } from '../../atoms/badge';
 import { Palette } from '../../atoms/palette';
 import { Spacer } from '../../atoms/spacer';
 import { Text } from '../../atoms/typography';
 import { BadgeBGColor } from '../../shared-types';
-import { BadgeContainer, TodoCardContainer } from './styles';
+import {
+  AvatarsContainer,
+  BadgeContainer,
+  TodoCardContainer,
+  TodoCardContent,
+  TodoCardHeader,
+} from './styles';
 
 interface TodoCardProps extends Omit<TodoProps, 'assigned'> {
-  assigned: UserProps[];
+  assigned?: string[];
+  showStatus?: boolean;
+  showDescription?: boolean;
 }
 
 export const TodoCard: FC<TodoCardProps> = ({
@@ -19,35 +26,63 @@ export const TodoCard: FC<TodoCardProps> = ({
   timestamp,
   status,
   description,
+  showStatus,
+  showDescription,
   assigned,
-}) => (
-  <TouchableOpacity>
+}) => {
+  const assignedAvatars = hooks.useFetchAvatar<typeof assigned>(assigned);
+
+  const avatars = useCallback(
+    () =>
+      assignedAvatars?.map((avatar, index) => (
+        <Avatar
+          key={index}
+          style={{
+            borderWidth: 1,
+            borderColor: Palette.greys.G200,
+            right: (index - 1) * 5,
+            position: 'relative',
+            zIndex: -index,
+          }}
+          source={{ uri: avatar.avatar }}
+          radius={'full'}
+          size={tokens.sizes.extraLarge}
+        />
+      )),
+    [assignedAvatars]
+  );
+
+  return (
     <TodoCardContainer
       cardColor={color as keyof typeof tokens.colours.light.todoPalette}
     >
-      <BadgeContainer>
-        <Badge status={status as keyof typeof BadgeBGColor} />
-      </BadgeContainer>
-      <Text size="large" weight="bold">
-        {todo}
-      </Text>
-      <Spacer size="8" />
-      <Spacer size="4" />
-      <Text size="small" weight="regular">
-        {description}
-      </Text>
-      <Spacer size="8" />
-      <Spacer size="4" />
-      <Text size="small" weight="bold" colour={Palette.greys.G200}>
-        {dateFormatter(timestamp)}
-      </Text>
-      {/* {assigned?.map(({ avatar }) => {
-        return (
-          <Avatar imageUrl={avatar} radius={'full'} size={tokens.sizes.small} />
-        );
-      })} */}
+      <TodoCardHeader>
+        <Text size="small" weight="medium" colour={Palette.greys.G200}>
+          {dateFormatter(timestamp)}
+        </Text>
+        {showStatus && (
+          <BadgeContainer>
+            <Badge status={status as keyof typeof BadgeBGColor} />
+          </BadgeContainer>
+        )}
+      </TodoCardHeader>
+      <TodoCardContent>
+        <Text size="large" weight="bold">
+          {todo}
+        </Text>
+        {showDescription && (
+          <>
+            <Spacer size="4" />
+            <Text size="small" weight="regular">
+              {description}
+            </Text>
+            <Spacer size="4" />
+          </>
+        )}
+      </TodoCardContent>
+      <AvatarsContainer>{avatars()}</AvatarsContainer>
     </TodoCardContainer>
-  </TouchableOpacity>
-);
+  );
+};
 
 TodoCard.displayName = 'TodoCard';
--- packages/mobile-ui/src/organisms/todo-card/styles.ts
+++ packages/mobile-ui/src/organisms/todo-card/styles.ts
@@ -2,20 +2,32 @@ import { View } from 'react-native';
 import { tokens } from '@todo/commons';
 import styled from 'styled-components/native';
 
-export const BadgeContainer = styled.View`
-  position: absolute;
-  right: 0;
-  top: -16px;
+export const BadgeContainer = styled.View``;
+
+export const AvatarsContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  margin-top: auto;
+  justify-content: flex-end;
+  align-items: flex-end;
 `;
+
+export const TodoCardHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TodoCardContent = styled.View``;
 
 export const TodoCardContainer = styled(View)<{
   cardColor?: keyof typeof tokens.colours.light.todoPalette;
 }>`
-  padding: ${(props) => props.theme.sizes.medium}px;
+  flex: 1;
+  padding: 24px 16px;
   background-color: ${(props) =>
     !props.cardColor
       ? props.theme.colours.primary.P100
       : props.theme.colours.todoPalette[props.cardColor]};
-  border-radius: ${(props) => props.theme.sizes.small / 2}px;
-  box-shadow: ${({ theme: { shadow } }) => shadow.L3};
+  border-radius: 20px;
 `;
