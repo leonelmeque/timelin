070c375bb678b5583da59b6caaac8c13f1df2ada
Leonel Meque <leomeque@gmail.com>
2023-01-24 12:14:16 +0100

    chore(mobile-ui): add more documentation on inputs

--- packages/mobile-ui/default.tsx
+++ packages/mobile-ui/default.tsx
@@ -1,4 +1,4 @@
-import React, { FC, PropsWithChildren, ReactNode, useState } from 'react';
+import React from 'react';
 import { hooks, theme } from '@todo/commons';
 import { ThemeProvider } from 'styled-components/native';
 import {
@@ -10,9 +10,10 @@ import {
   TouchableWithoutFeedback,
   View,
 } from 'react-native';
-import { Text, Spacer, Input, FormInput } from './src';
+import { Text, Spacer } from './src';
 import { ButtonPage } from './src/presentations/pages/button-page';
 import { Container } from './src/presentations/components/container';
+import { InputPage } from './src/presentations/pages/input-page';
 
 const TypographyScreen = () => (
   <Container>
@@ -34,65 +35,6 @@ const TypographyScreen = () => (
   </Container>
 );
 
-const InputScreen = () => {
-  const [form, setForm] = useState<{ [key: string]: any }>({});
-
-  const onFormChange = ({ name, value }: { name: string; value: string }) => {
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  return (
-    <Container>
-      <Text size="heading" weight="bold">
-        Input
-      </Text>
-      <Spacer size={'4'} />
-      <Input placeholder="Placeholder" value="Input has text" />
-      <Spacer size={'4'} />
-      <Input placeholder="Default state" />
-      <Spacer size={'4'} />
-      <Input
-        value={form?.form1 || ''}
-        placeholder="Error State"
-        hasError={!form.form1?.length}
-        onChangeText={(val) => onFormChange({ name: 'form1', value: val })}
-      />
-      <Spacer size={'4'} />
-      <Input placeholder="Disabled state" numberOfLines={5} disabled editable />
-      <Spacer size={'16'} />
-      <FormInput label="Label" />
-      <Spacer size={'8'} />
-      <FormInput
-        placeholder="mail@domain.com"
-        label="Email"
-        captionText="We will never share your email"
-        variant="caption"
-        keyboardType="email-address"
-      />
-      <Spacer size={'8'} />
-      <FormInput
-        label="Username"
-        value="jhon_doe"
-        captionText="Your user must not contain special characters"
-        variant="success"
-        errorText="Please add a valid user name."
-        successText="Nice work"
-      />
-      <Spacer size={'8'} />
-      <FormInput
-        label="Username"
-        value="app"
-        captionText="Your user must not contain special characters"
-        variant="error"
-        errorText="Please add a valid user name."
-      />
-    </Container>
-  );
-};
-
 export const Default = () => {
   const { useThemeSwitcher } = hooks;
   const [selectedTheme] = useThemeSwitcher();
@@ -108,7 +50,7 @@ export const Default = () => {
               <View>
                 <ButtonPage />
                 <TypographyScreen />
-                <InputScreen />
+                <InputPage />
               </View>
             </TouchableWithoutFeedback>
           </ScrollView>
