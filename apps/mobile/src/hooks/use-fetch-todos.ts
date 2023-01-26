50e60660ebb5dcf62fe9a52d1ed229e61ce0605f
Leonel Meque <leomeque@gmail.com>
2023-01-26 10:12:49 +0100

    feat(mobile): home page refeches on focus

--- apps/mobile/src/hooks/use-fetch-todos.ts
+++ apps/mobile/src/hooks/use-fetch-todos.ts
@@ -2,14 +2,14 @@ import { TodoProps } from '@todo/commons';
 import { useEffect, useState } from 'react';
 import { todosAPI } from '../utils/backend';
 
-const useFetchTodos = () => {
+const useFetchTodos = (shouldRefresh?: boolean) => {
   const [state, setState] = useState<TodoProps[] | null>(null);
 
   useEffect(() => {
     todosAPI.getTodos().then((data) => {
       setState(data);
     });
-  }, []);
+  }, [shouldRefresh]);
 
   return state;
 };
