9f329893c98489e6f68b4398bc1b4c6f339d300a
Leonel Meque <leomeque@gmail.com>
2023-02-01 10:41:58 +0100

    fix(commons): date formatter now accepts number or string

--- packages/commons/src/utils/date-formatter.ts
+++ packages/commons/src/utils/date-formatter.ts
@@ -1,6 +1,6 @@
-export const dateFormatter = (timestamp: string) =>
+export const dateFormatter = (timestamp: string | number) =>
   new Intl.DateTimeFormat('en-US', {
     month: 'long',
     day: '2-digit',
     year: 'numeric',
-  }).format(new Date(Number(timestamp)));
+  }).format(new Date(timestamp));
