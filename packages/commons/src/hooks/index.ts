bb3ce437be7f26609a57b167299839f0ec75f634
Leonel Meque <leomeque@gmail.com>
2023-02-05 15:23:51 +0100

    feat(commons): add hook to fetch timeline

--- packages/commons/src/hooks/index.ts
+++ packages/commons/src/hooks/index.ts
@@ -4,6 +4,7 @@ import { useSearchTodos } from './use-search-todos';
 import { useFetchAvatar } from './use-fetch-avatar';
 import { useFilterByStatus } from './use-filter-by-status';
 import { useUpdateTodo } from './use-update-todo';
+import { useFetchTimeline } from './use-fetch-timeline';
 
 export const hooks = {
   useGreetings,
@@ -12,4 +13,5 @@ export const hooks = {
   useFetchAvatar,
   useFilterByStatus,
   useUpdateTodo,
+  useFetchTimeline,
 };
