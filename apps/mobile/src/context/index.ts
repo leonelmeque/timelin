0be0cc265dba7d8786cc1a85d4f626d68630d703
Leonel Meque <leomeque@gmail.com>
2023-02-05 23:15:41 +0100

    refactor(mobile): remove dead code

--- apps/mobile/src/context/index.ts
+++ apps/mobile/src/context/index.ts
@@ -1,5 +1,3 @@
-export { Provider as ThemeContext } from './theme-context'
 export { Provider as CustomModalProvider } from './modal-context';
-export { Provider as TodosProvider } from './todos-context'
 
 export { useCustomModal } from './modal-context';
