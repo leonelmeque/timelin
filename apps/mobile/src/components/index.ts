71ff8855816f82a04b8fd775ea5e9de249d6a9a9
Leonel Meque <leomeque@gmail.com>
2023-02-05 23:12:33 +0100

    fix(mobile): removed unnecessary files

--- apps/mobile/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import Button from './atoms/Button/Button';
-import Typography from './atoms/Typography/typography';
-import { Header } from './molecules/Header/header';
-import TextButton from './atoms/TextButton/TextButton';
-
-export { Button, Typography, Header, TextButton };
