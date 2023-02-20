a32caa9304f97617e0b06fea83e926661d2345f3
Leonel Meque <leomeque@gmail.com>
2023-02-20 20:30:28 +0100

    feat(backend): add logger middleware

--- apps/backend/src/dev.server.ts
+++ apps/backend/src/dev.server.ts
@@ -1,5 +1,8 @@
 import express from 'express';
-import todosRouter from './routes/todos';
+
+import todosRouter from './routes/todos.route';
+import timelineRouter from './routes/timeline.route';
+import { logger } from './middleware/logger';
 
 const app = express();
 
@@ -7,12 +10,9 @@ app.use(express.json());
 
 const PORT = 3002;
 
-app.get('/', (_req, res) => {
-  console.log('someone pinged here!!');
-  res.send('Hello world');
-});
-
+app.use(logger);
 app.use('/api/todos', todosRouter);
+app.use('/api/timeline', timelineRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
