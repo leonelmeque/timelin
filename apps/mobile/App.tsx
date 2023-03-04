ae89ae96eefbb5b29ca44279ef5b11bd33f98e20
Leonel Meque <leomeque@gmail.com>
2023-03-04 21:55:58 +0100

    feat(commons): add user login feature

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,24 +1,38 @@
-import { ThemeProvider } from 'styled-components/native'
-import { NavigationContainer } from '@react-navigation/native'
+import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { hooks, theme } from '@todo/commons';
 import { Tabs } from './src/routes/tab-routes';
-import { CustomModalProvider } from './src/context';
+import {
+  AuthenticatedUserProvider,
+  CustomModalProvider,
+  useUserContext,
+} from './src/context';
 import AuthStack from './src/routes/auth.routes';
+import { useGetSessionStorage } from './src/hooks/useGetSessionToken';
+import { FC } from 'react';
+
+const IsUserAuthenticated: FC<any> = () => {
+  useGetSessionStorage();
+  const [user] = useUserContext();
+
+  return user ? <Tabs /> : <AuthStack />;
+};
 
 export default function App() {
   const [activeTheme] = hooks.useThemeSwitcher();
+
   return (
-    <ThemeProvider
-      theme={activeTheme === 'light' ? theme[activeTheme] : theme[activeTheme]}
-    >
-      <CustomModalProvider>
-        <SafeAreaProvider>
-          <NavigationContainer>
-            <AuthStack />
-          </NavigationContainer>
-        </SafeAreaProvider>
-      </CustomModalProvider>
+    <ThemeProvider theme={theme[activeTheme]}>
+      <AuthenticatedUserProvider>
+        <CustomModalProvider>
+          <SafeAreaProvider>
+            <NavigationContainer>
+              <IsUserAuthenticated />
+            </NavigationContainer>
+          </SafeAreaProvider>
+        </CustomModalProvider>
+      </AuthenticatedUserProvider>
     </ThemeProvider>
   );
 }
