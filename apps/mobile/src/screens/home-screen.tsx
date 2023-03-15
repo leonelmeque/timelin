9424dddc31c5a903d598d8be345e3181119fbed9
Leonel Meque <leomeque@gmail.com>
2023-03-15 21:27:27 +0100

    fix(mobile): home screen sections

--- apps/mobile/src/screens/home-screen.tsx
+++ apps/mobile/src/screens/home-screen.tsx
@@ -1,20 +1,49 @@
 import { Spacer, Header, Text, Avatar, Palette, Box } from '@todo/mobile-ui';
 import { TodoListView } from '../components/todo-list-view';
 import { CustomSafeAreaView } from '../components/safe-area-view';
-import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import { Keyboard, Pressable } from 'react-native';
 import { hooks, tokens } from '@todo/commons';
 import { AddTodoModalView } from '../components/add-todo-modal-view';
 import { useCustomModal, useUserContext } from '../context';
-import { useCallback, useState } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
+import styled from 'styled-components/native';
+
+const SafeArea = styled(CustomSafeAreaView)`
+  flex: 1;
+  background-color: #fff;
+`;
+
+const SectionHeader = styled(Box)`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0px;
+`;
+
+const SectionContent = styled.View`
+  padding: 0px;
+`;
+
+const Section = styled(Box)`
+  flex: 1;
+`;
 
 export default function HomeScreen() {
+  const [user] = useUserContext();
   const navigation = useNavigation();
   const [modalVisibility, setModalVisibility] = useCustomModal();
-  const [user] = useUserContext();
-  // const [shouldRefresh, setShouldRefresh] = useState(false);
-  const todos = hooks.useFetchTodos(user?.todos as string[]);
+  const { todos, isLoading } = hooks.useFetchTodos(
+    (user?.todos as string[]) || []
+  );
+
+  const nagivateToTodoList = () =>
+    //@ts-ignore
+    navigation.navigate<string>('Todo/ListTodo', { todos });
+
+  const onModalDismiss = () => {
+    Keyboard.dismiss();
+    setModalVisibility(!modalVisibility);
+  };
 
   const renderRigthContent = () => (
     <Pressable
@@ -35,31 +64,23 @@ export default function HomeScreen() {
     />
   );
 
-  const nagivateToTodoList = () =>
-    //@ts-ignore
-    navigation.navigate<string>('Todo/ListTodo', { todos });
-
-  const onModalDismiss = () => {
-    Keyboard.dismiss();
-    setModalVisibility(!modalVisibility);
-  };
-
-  // useFocusEffect(
-  //   useCallback(() => {
-  //     setShouldRefresh(true);
-  //     return () => {
-  //       setShouldRefresh(false);
-  //     };
-  //   }, [])
-  // );
+  if (isLoading === 'FETCHING')
+    return (
+      <SafeArea>
+        <Box
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text size="heading">Loading data</Text>
+        </Box>
+      </SafeArea>
+    );
 
   return (
-    <CustomSafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: '#FFF',
-      }}
-    >
+    <SafeArea>
       <AddTodoModalView
         key={todos?.length}
         visibility={modalVisibility}
@@ -70,36 +91,62 @@ export default function HomeScreen() {
         renderRigthContent={renderRigthContent}
       />
       <Spacer size="8" />
-      <Box
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <Text size="body" weight="medium">
-          Projects
-        </Text>
-        <Pressable onPress={nagivateToTodoList}>
-          <Text size="small" weight="medium" colour={Palette.primary.P300}>
-            More
+
+      <Section accessibilityLabel="Projects">
+        <SectionHeader>
+          <Text size="body" weight="medium">
+            Projects
           </Text>
-        </Pressable>
-      </Box>
+          <Pressable onPress={nagivateToTodoList}>
+            <Text size="small" weight="medium" colour={Palette.primary.P300}>
+              More
+            </Text>
+          </Pressable>
+        </SectionHeader>
+        <Spacer size="4" />
+        <SectionContent>
+          <>
+            {!!todos && (
+              <Text size="small">Looks like you don't have any projects</Text>
+            )}
+            {todos && <TodoListView horizontal data={todos} />}
+          </>
+        </SectionContent>
+      </Section>
       <Spacer size="4" />
-      {!todos && (
-        <Box
-          style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <Text size="body">Looks like you don't have any projects</Text>
-        </Box>
-      )}
-      {todos && <TodoListView horizontal data={todos} />}
-    </CustomSafeAreaView>
+      <Section accessibilityLabel="Pinned">
+        <SectionHeader>
+          <Text size="body" weight="medium">
+            Pinned
+          </Text>
+        </SectionHeader>
+        <Spacer size="4" />
+        <SectionContent>
+          <>
+            {!!todos && (
+              <Text size="small">You can pin projects for easy access</Text>
+            )}
+          </>
+        </SectionContent>
+      </Section>
+      <Spacer size="4" />
+      <Section accessibilityLabel="Latest">
+        <SectionHeader>
+          <Text size="body" weight="medium">
+            Latest
+          </Text>
+        </SectionHeader>
+        <Spacer size="4" />
+        <SectionContent>
+          <>
+            {!!todos && (
+              <Text size="small">There are no recent projects created</Text>
+            )}
+          </>
+        </SectionContent>
+      </Section>
+      <Spacer size="4" />
+    </SafeArea>
   );
 }
 
