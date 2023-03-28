3eac3824ed543c6403b5dec5e8eee28353168b34
Leonel Meque <leomeque@gmail.com>
2023-03-28 07:12:16 +0200

    feat(store): add jotai store package

---
+++ packages/store/src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useFetchTodos } from './use-fetch-todos';
+export { useSearchTodos } from './use-search-todos';
+export { useFetchTodo } from './use-fetch-todo';
+export { useFilterTodosStatus } from './use-filter-todos-status';
+export { useUpdateTodos } from './use-update-todos';
