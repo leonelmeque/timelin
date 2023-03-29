f74cb87b46d0e0debbc140cfba70382f4eacf376
Leonel Meque <leomeque@gmail.com>
2023-03-29 07:53:52 +0200

    fix: build errors

--- packages/commons/src/hooks/index.ts
+++ packages/commons/src/hooks/index.ts
@@ -2,12 +2,10 @@ import { useGreetings } from './use-greetings';
 import { useThemeSwitcher } from './use-theme-switcher';
 import { useFetchAvatar } from './use-fetch-avatar';
 import { useFetchTimeline } from './use-fetch-timeline';
-import { useFetchTodo } from './use-fetch-todo';
 
 export const hooks = {
   useGreetings,
   useThemeSwitcher,
   useFetchAvatar,
   useFetchTimeline,
-  useFetchTodo,
 };
