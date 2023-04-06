1467365a3cb26730e1f6c17a02de575957637944
Leonel Meque <leomeque@gmail.com>
2023-04-06 18:23:50 +0200

    chore(commons): code sweaping

--- packages/commons/src/api/index.ts
+++ packages/commons/src/api/index.ts
@@ -1,6 +1,6 @@
 import { todo } from './todo';
-import * as timeline from './timeline';
-import * as users from './users';
+import { timeline } from './timeline';
+import { users } from './users';
 
 export const api = {
   todo,
