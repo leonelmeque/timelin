d2cd9c9e55f726abb2ff9b718bf0bf300b743e39
Leonel Meque <leomeque@gmail.com>
2023-04-06 18:14:52 +0200

    refactor(commons): split into files todo api functions

---
+++ packages/commons/src/api/todo/index.ts
@@ -0,0 +1,14 @@
+import { createTodo } from './create-todo';
+import { deleteTodo } from './delete-todo';
+import { getTodo } from './get-todo';
+import { getTodos } from './get-todos';
+import { updateTodo } from './update-todo';
+
+
+export const todo = {
+  createTodo,
+  deleteTodo,
+  getTodo,
+  getTodos,
+  updateTodo
+};
