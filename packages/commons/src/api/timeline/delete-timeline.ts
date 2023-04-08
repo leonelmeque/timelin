b540fd6de118d507ffdafbc006a33636428de604
Leonel Meque <leomeque@gmail.com>
2023-04-08 21:24:52 +0200

    feat(commons): add firebase to timeline ops

--- packages/commons/src/api/timeline/delete-timeline.ts
+++ packages/commons/src/api/timeline/delete-timeline.ts
@@ -1,11 +1,9 @@
-import { TIMELINE_URL, headers } from "../../utils/constants";
+import firebase from 'firebase';
 
 export const deleteTimeline = async (id: string) => {
-  const resp = await fetch(`${TIMELINE_URL}/remove/all/${id}`, {
-    method: 'DELETE',
-    headers,
-  });
-  const data = await resp.json();
-
-  return data;
+  return await firebase
+    .firestore()
+    .collection('timeline')
+    .doc(firebase.auth()?.currentUser?.uid)
+    .delete();
 };
