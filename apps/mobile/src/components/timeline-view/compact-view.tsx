2c7ad8c1e5ef7ff6e8feb853e8e58e5ed49315a5
Leonel Meque <leomeque@gmail.com>
2023-04-11 13:33:53 +0200

    fix(mobile): timeline and todo screens

--- apps/mobile/src/components/timeline-view/compact-view.tsx
+++ apps/mobile/src/components/timeline-view/compact-view.tsx
@@ -1,55 +1,72 @@
-import { hooks } from '@todo/commons';
-import { Spacer } from '@todo/mobile-ui';
-import { useEffect, useState } from 'react';
-import { FlatList } from 'react-native';
+import { eventsDateSorter } from '@todo/commons';
+import { Palette, Spacer, Text } from '@todo/mobile-ui';
+import { useFetchTimeline, useTimeline } from '@todo/store';
+import { useMemo, useRef } from 'react';
+import { FlatList, Pressable } from 'react-native';
 import { RenderTimelineEvent } from './render-timeline-event';
+import { useNavigation } from '@react-navigation/native';
 
 export const TimelineCompactView = ({ id }: { id: string }) => {
-  const [timeline] = hooks.useFetchTimeline(id as string);
-  const [state, setState] = useState<any[] | null>(null);
+  const { value } = useFetchTimeline(id);
+  const { timeline } = useTimeline();
+  const navigation = useNavigation();
 
-  useEffect(() => {
-    if (state) return;
+  const ref = useRef(1);
 
-    const arr: any[] = [];
-
-    if (timeline) {
-      const timelineKeys = Object.keys(timeline).slice(
-        Object.keys(timeline).length - 4,
-        Object.keys(timeline).length
-      );
-
-      if (timelineKeys.length >= 1 && timeline) {
-        timelineKeys.forEach((_date) => {
-          /**
-           * TODO: fix this TS error
-           */
-          //@ts-ignore
-          arr.push(timeline[_date].slice(-1)[0]);
-        });
-
-        setState(arr);
-      }
+  const normalizeData = useMemo(() => {
+    if (value.state === 'loading' || value.state === 'hasError') {
+      return [];
     }
-  });
 
-  if (!state?.length) return <></>;
+    const arr = timeline.slice(timeline.length - 4, timeline.length);
+
+    if (!timeline.length) {
+      ref.current = arr.length;
+    }
+
+    return eventsDateSorter(arr);
+  }, [value]);
+
+  if (value.state === 'loading') return <Text size="body">Loading data</Text>;
+  if (value.state === 'hasError')
+    return <Text size="body">Error loading data</Text>;
 
   return (
     <>
-      <FlatList
-        data={state}
-        ItemSeparatorComponent={() => <Spacer size="8" />}
-        renderItem={({ item, index }) => (
-          <RenderTimelineEvent
-            date={Number(item.timestamp)}
-            description={item.description}
-            compact
-            showVerticalLine={state?.length - 1 !== index}
-          />
-        )}
-      />
-      <Spacer size="8" />
+      <Pressable
+        onPress={() => {
+          //@ts-ignore
+          navigation.navigate<any>('Timeline/Default', {
+            todoUID: id,
+          });
+        }}
+      >
+        <FlatList
+          data={normalizeData}
+          ItemSeparatorComponent={() => <Spacer size="8" />}
+          renderItem={({ item, index }) => (
+            <RenderTimelineEvent
+              date={Number(item.timestamp)}
+              description={item.title}
+              compact
+              showVerticalLine={ref.current - 2 !== index}
+            />
+          )}
+        />
+      </Pressable>
+      {normalizeData.length !== 0 && <Spacer size="8" />}
+      <Pressable
+        onPress={() => {
+          //@ts-ignore
+          navigation.navigate<any>('Timeline/AddEvent', {
+            todoUID: id,
+          });
+        }}
+      >
+        <Text size="body" weight="medium" colour={Palette.primary.P300}>
+          + Add new timeline event
+        </Text>
+      </Pressable>
     </>
   );
 };
