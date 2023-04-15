bfd00f1792f0bc3110a2e12db75af25e84685771
Leonel Meque <leomeque@gmail.com>
2023-04-16 00:05:47 +0200

    fix(commons): improved auth functions

--- packages/commons/src/api/users/user-signin.ts
+++ packages/commons/src/api/users/user-signin.ts
@@ -1,11 +1,12 @@
 import { UserLogin } from '../../shared-types';
 import { getUserInformation } from './get-user-information';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 
 export const userSignIn = async ({
   username,
   password,
 }: Partial<UserLogin>) => {
+  await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
   const ref = await firebase
     .auth()
     .signInWithEmailAndPassword(username as string, password as string);
