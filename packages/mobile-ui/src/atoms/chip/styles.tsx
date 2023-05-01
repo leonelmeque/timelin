0d3cb7c9bdf53a86a43733975ee7a16b86573787
Leonel Meque <leomeque@gmail.com>
2023-05-01 19:07:59 +0200

    fix: removed mobile-ui package

--- packages/mobile-ui/src/atoms/chip/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native';
-
-export const Container = styled.View<{ colour: string }>`
-  background-color: ${(props) => props.colour};
-  padding: 10px;
-  align-items: center;
-  justify-content: center;
-  border-radius: 8px;
-`;
--- packages/mobile-ui/src/declations.d.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultTheme as Theme } from '@todo/commons/dist/theme/types';
-
-declare module 'styled-components/native' {
-  interface DefaultTheme extends Theme {}
-}
-declare module 'styled-components' {
-  interface DefaultTheme extends Theme {}
-}
