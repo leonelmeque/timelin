12b83d24c67ab0154970c846e81f848e08912701
Leonel Meque <leomeque@gmail.com>
2023-05-01 19:19:24 +0200

    fix: remove store package

--- packages/store/src/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { useFetchTodos } from './use-fetch-todos';
-export { useSearchTodos } from './use-search-todos';
-export { useFetchTodo } from './use-fetch-todo';
-export { useFilterTodosStatus } from './use-filter-todos-status';
-export { useUpdateTodos } from './use-update-todos';
-export { useFetchTimeline } from './use-fetch-timeline';
-export { useTimeline } from './use-timeline';
-export { usePinnedTodo } from './use-pinned-todo';
