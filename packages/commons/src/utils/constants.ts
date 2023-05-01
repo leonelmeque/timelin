b9bd6a57bf7dfaa1d03ecb4c5d3a4d14b4d74c54
Leonel Meque <leomeque@gmail.com>
2023-05-01 19:07:40 +0200

    fix: removed commons package

--- packages/commons/src/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-const API_URL = import.meta.env.VITE_TODO_API;
-
-export const TODO_URL = `${API_URL}/todos`;
-export const TIMELINE_URL = `${API_URL}/timeline`;
-export const USERS_URL = `${API_URL}/users`;
-
-export const headers = {
-  'Content-Type': 'application/json',
-};
