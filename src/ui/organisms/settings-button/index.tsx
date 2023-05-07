f7563d3228ecbf560beb813fd7181746f154476c
Leonel Meque <leomeque@gmail.com>
2023-05-07 16:09:24 +0200

    feat: add settings button

---
+++ src/ui/organisms/settings-button/index.tsx
@@ -0,0 +1,54 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { Container, MiddleContent, RightContent } from './styles';
+import { useTheme } from 'styled-components/native';
+import { Spacer, Text } from '../../atoms';
+import { ComponentProps, FC } from 'react';
+
+type IconNames = ComponentProps<typeof MaterialIcons>['name']
+
+type SettingsButtonProps = {
+  iconName: IconNames;
+  settingName: string;
+  description?: string;
+  withToggle?: boolean;
+  rightContent?: React.ReactNode;
+};
+
+export const SettingsButton: FC<SettingsButtonProps> = ({
+  iconName = "america",
+  settingName,
+  description,
+  rightContent
+}) => {
+  const theme = useTheme();
+  return (
+    <Container>
+      <MaterialIcons
+        name={iconName as any}
+        size={24}
+        color={theme.colours.neutrals.dark}
+      />
+      <MiddleContent>
+        <Text size="body" weight='bold'>
+          {settingName}
+        </Text>
+        {
+          description && (
+            <>
+              <Spacer size='4' />
+              <Text size="small">
+                {description}
+              </Text>
+            </>
+          )
+        }
+      </MiddleContent>
+      {
+        rightContent &&
+        <RightContent>
+          {rightContent}
+        </RightContent>
+      }
+    </Container>
+  );
+};
