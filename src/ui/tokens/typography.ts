85615497ab93dd6e3aa7fc47170721fc829a392d
Leonel Meque <leomeque@gmail.com>
2023-05-15 21:15:15 +0200

    fix: typography component

--- src/ui/tokens/typography.ts
+++ src/ui/tokens/typography.ts
@@ -7,6 +7,21 @@ const sizes: DefaultTheme['typography']['sizes'] = {
   heading: 24,
 };
 
+const weight: DefaultTheme['typography']['weight'] = {
+  '100': 100,
+  '200': 200,
+  '300': 300,
+  '400': 400,
+  '500': 500,
+  '600': 600,
+  medium: 500,
+  '800': 800,
+  '900': 900,
+  bold: 'bold',
+  regular: 'normal',
+};
+
 export const typography = {
   sizes,
+  weight
 };
