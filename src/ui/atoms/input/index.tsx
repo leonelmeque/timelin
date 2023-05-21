b87dea139f6eb1036246a9150b0266b23d1f3956
Leonel Meque <leomeque@gmail.com>
2023-05-21 13:39:18 +0200

    feat(ui): add forwarded ref to components

--- src/ui/atoms/input/index.tsx
+++ src/ui/atoms/input/index.tsx
@@ -1,65 +1,64 @@
-import { FC } from 'react';
+import { forwardRef } from "react";
 import {
   NativeSyntheticEvent,
+  TextInput,
   TextInputChangeEventData,
   TextInputFocusEventData,
   TextInputProps,
-} from 'react-native';
-import { Palette } from '../palette';
-import { InputVariant, StyledInput } from './styles';
+} from "react-native";
+import { Palette } from "../palette";
+import { InputVariant, StyledInput } from "./styles";
 
-type InputProps = {
+interface InputProps extends TextInputProps {
   disabled?: boolean;
   hasError?: boolean;
   success?: boolean;
-};
+}
 
-export const Input: FC<TextInputProps & InputProps> = ({
-  hasError,
-  disabled,
-  success,
-  ...rest
-}) => {
-  let variant;
+export const Input = forwardRef<TextInput, InputProps>(
+  ({ hasError, disabled, success, ...rest }, ref) => {
+    let variant;
 
-  const onChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
-    if (!hasError) {
+    const onChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+      if (!hasError) {
+        variant = InputVariant.FOCUS;
+      }
+    };
+
+    const onFocus = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
+      if (rest.onFocus) rest?.onFocus(e);
       variant = InputVariant.FOCUS;
-    }
-  };
+    };
 
-  const onFocus = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
-    if (rest.onFocus) rest?.onFocus(e);
-    variant = InputVariant.FOCUS;
-  };
+    const onBlur = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
+      if (rest.onBlur) rest.onBlur(e);
+      variant = InputVariant.DEFAULT;
+    };
 
-  const onBlur = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
-    if (rest.onBlur) rest.onBlur(e);
-    variant = InputVariant.DEFAULT;
-  };
+    const setInputVariant = () => {
+      if (hasError) {
+        return InputVariant.ERROR;
+      } else if (disabled) {
+        return InputVariant.DISABLED;
+      } else if (success) {
+        return InputVariant.SUCCESS;
+      }
+      return InputVariant.DEFAULT;
+    };
 
-  const setInputVariant = () => {
-    if (hasError) {
-      return InputVariant.ERROR;
-    } else if (disabled) {
-      return InputVariant.DISABLED;
-    } else if (success) {
-      return InputVariant.SUCCESS;
-    }
-    return InputVariant.DEFAULT;
-  };
+    variant = setInputVariant();
 
-  variant = setInputVariant();
-
-  return (
-    <StyledInput
-      {...rest}
-      variant={variant}
-      onFocus={onFocus}
-      onBlur={onBlur}
-      editable={!disabled}
-      onChange={onChange}
-      placeholderTextColor={Palette.greys.G75}
-    />
-  );
-};
+    return (
+      <StyledInput
+        ref={ref}
+        variant={variant}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        editable={!disabled}
+        onChange={onChange}
+        placeholderTextColor={Palette.greys.G75}
+        {...rest}
+      />
+    );
+  }
+);
