339fd8e9f90dbae16fa0b658fdb08ab0a39105f4
Leonel Meque <leomeque@gmail.com>
2023-05-28 23:17:00 +0200

    fix(components): add keyboard view

--- src/components/account-settings-modal-view/index.tsx
+++ src/components/account-settings-modal-view/index.tsx
@@ -1,44 +1,55 @@
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { Box, Spacer, Text } from '../../ui/atoms';
-import { CustomSafeAreaView } from '../safe-area-view';
-import { MaterialIcons } from '@expo/vector-icons';
-import { ChangePasswordForm } from './change-password-form';
-import { KeyboardAvoidingView } from 'react-native';
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import { Box, Spacer, Text } from "../../ui/atoms";
+import { CustomSafeAreaView } from "../safe-area-view";
+import { MaterialIcons } from "@expo/vector-icons";
+import { ChangePasswordForm } from "./change-password-form";
+import { KeyboardAvoidingView, Platform, ScrollView } from "react-native";
+import { useCallback } from "react";
+
+type RouteParams = {
+  params: {
+    value: string;
+  };
+};
 
 export const AccountSettingsModalView = () => {
   const {
     params: { value },
-  } = useRoute() as any;
+  } = useRoute<RouteProp<RouteParams>>();
 
   const navigation = useNavigation();
 
-  const RenderFrom = () => {
-    if (value === 'change-password') {
+  const RenderFrom = useCallback(() => {
+    if (value === "change-password") {
       return <ChangePasswordForm />;
-    } else if (value === 'delete-account') {
+    } else if (value === "delete-account") {
       return <></>;
     }
     return <></>;
-  };
+  }, []);
 
   return (
     <CustomSafeAreaView>
-      <KeyboardAvoidingView>
-        <Box>
-          <Spacer size="8" />
-          <MaterialIcons
-            name="close"
-            size={24}
-            color="black"
-            onPress={() => navigation.goBack()}
-          />
-          <Spacer size="8" />
-          <Text size="heading" weight="bold">
-            Change password
-          </Text>
-          <RenderFrom />
-        </Box>
-      </KeyboardAvoidingView>
+      <Box style={{ backgroundColor: "white", flex: 1 }}>
+        <Spacer size="8" />
+        <MaterialIcons
+          name="close"
+          size={24}
+          color="black"
+          onPress={() => navigation.goBack()}
+        />
+        <Spacer size="8" />
+        <Text size="heading" weight="bold">
+          Change password
+        </Text>
+        <ScrollView>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+          >
+            <RenderFrom />
+          </KeyboardAvoidingView>
+        </ScrollView>
+      </Box>
     </CustomSafeAreaView>
   );
 };
