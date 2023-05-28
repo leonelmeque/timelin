5373b4feb792351b67f802ad3ff360ff4454882f
Leonel Meque <leomeque@gmail.com>
2023-05-28 23:17:20 +0200

    fix(screens): add keyboard view

--- src/screens/auth/signin-screen.tsx
+++ src/screens/auth/signin-screen.tsx
@@ -1,28 +1,32 @@
-import { useNavigation } from '@react-navigation/native';
-import { Dimensions, Image } from 'react-native';
-import styled from 'styled-components/native';
-import { CustomSafeAreaView } from '../../components/safe-area-view';
-import { SignInFormView } from '../../components/signin-form-view';
-import { useUserContext } from '../../context';
-import { useState } from 'react';
-import { User, api } from '../../lib';
+import { useNavigation } from "@react-navigation/native";
+import {
+  Dimensions,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
+import styled from "styled-components/native";
+import { CustomSafeAreaView } from "../../components/safe-area-view";
+import { SignInFormView } from "../../components/signin-form-view";
+import { useUserContext } from "../../context";
+import { useState } from "react";
+import { User, api } from "../../lib";
 
-const WIDTH = Dimensions.get('window').width;
-const HEIGHT = Dimensions.get('window').height;
+const WIDTH = Dimensions.get("window").width;
+const HEIGHT = Dimensions.get("window").height;
 
 const Container = styled.View`
-  flex: 1;
-  padding-top: 60px;
-  padding-bottom: ${HEIGHT / 4}px;
-  position: absolute;
-  top: ${HEIGHT / 3}px;
+  padding-top: ${(props) => props.theme.spacing.size56}px;
+  padding-bottom: ${HEIGHT / 8}px;
+  align-self: flex-end;
   width: ${WIDTH}px;
   border-top-right-radius: ${(props) => props.theme.spacing.size32}px;
   border-top-left-radius: ${(props) => props.theme.spacing.size32}px;
   background-color: ${(props) => props.theme.colours.neutrals.white};
 `;
 
-const bg = require('../../../assets/bg-login.jpg');
+const bg = require("../../../assets/bg-login.jpg");
 
 export const SignInScreen = () => {
   const navigation = useNavigation();
@@ -52,7 +56,7 @@ export const SignInScreen = () => {
           fullname: displayName,
         } as User);
       } else {
-        alert('Unable to login. Please try again.');
+        alert("Unable to login. Please try again.");
       }
     } catch (error) {
       console.error(error);
@@ -62,19 +66,31 @@ export const SignInScreen = () => {
   };
 
   return (
-    <CustomSafeAreaView>
-      <Image
-        source={bg}
-        style={{ width: WIDTH, height: HEIGHT, position: 'absolute' }}
-      />
-      <Container>
-        <SignInFormView
-          isLoggingIn={isLoggingIn}
-          onSignin={(data) => handleSignIn(data)}
-          //@ts-ignore
-          goToSignup={() => navigation.navigate('Todo/Signup')}
+    <ScrollView style={{ flexDirection: "row" }} bounces={false}>
+      <CustomSafeAreaView>
+        <Image
+          source={bg}
+          style={{ width: WIDTH, height: HEIGHT / 2, position: "absolute", resizeMode: 'stretch' }}
         />
-      </Container>
-    </CustomSafeAreaView>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{
+            // backgroundColor: 'red',
+            flex: 1,
+            alignSelf: "flex-end",
+            flexDirection: "row",
+          }}
+        >
+          <Container>
+            <SignInFormView
+              isLoggingIn={isLoggingIn}
+              onSignin={(data) => handleSignIn(data)}
+              //@ts-ignore
+              goToSignup={() => navigation.navigate("Todo/Signup")}
+            />
+          </Container>
+        </KeyboardAvoidingView>
+      </CustomSafeAreaView>
+    </ScrollView>
   );
 };
