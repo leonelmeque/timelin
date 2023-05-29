0a24386cd7e9df8471f32c566071928de8dc8ea7
Leonel Meque <leomeque@gmail.com>
2023-05-29 18:55:54 +0200

    fix(lib): user persistence login

--- src/lib/api/users/user-signin-with-persistence.ts
+++ src/lib/api/users/user-signin-with-persistence.ts
@@ -1,29 +1,39 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import { getUserInformation } from "./get-user-information";
+import { User } from "../../shared-types";
+import { Dispatch, SetStateAction } from "react";
 
-export const userSignInWithPersistence = () => {
-  return new Promise((resolve, reject) => {
-    const unsubscribe = firebase.auth().onAuthStateChanged(
-      (user) => {
-        if (user) {
-          getUserInformation(user.uid)
-            .then((user) => {
-              resolve(user);
-            })
-            .catch((err) => {
-              unsubscribe();
-              reject(err);
-            });
-        } else {
-          unsubscribe();
-          resolve(null);
-        }
-      },
-      (err) => {
-        unsubscribe();
-        reject(err);
-      }
+export const userSignInWithPersistence = (
+  callback: Dispatch<SetStateAction<User<{}> | null>>,
+  onAppReady: Dispatch<SetStateAction<boolean>>
+) => {
+  const onStateObserver = async (user: firebase.User | null) => {
+    if (!user) {
+      onAppReady(true);
+      return;
+    }
+
+    const userInformation = await getUserInformation(user.uid);
+    const { displayName, email, photoURL } = user;
+
+    callback(
+      () =>
+      ({
+        ...userInformation,
+        avatar: photoURL,
+        fullname: displayName,
+        email,
+      } as User<{}>)
     );
-  });
+
+    onAppReady(true);
+  };
+
+  const onStateObserverError = (err: firebase.auth.Error) => {
+    onAppReady(true);
+    throw err;
+  };
+
+  firebase.auth().onAuthStateChanged(onStateObserver, onStateObserverError);
 };
