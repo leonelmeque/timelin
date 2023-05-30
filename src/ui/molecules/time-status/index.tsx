affc7262e064fa8b787d471b249654c259265ca2
Leonel Meque <leomeque@gmail.com>
2023-05-30 09:07:02 +0200

    feat(ui): add translations to time status

--- src/ui/molecules/time-status/index.tsx
+++ src/ui/molecules/time-status/index.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react';
-import { Palette } from '../../atoms/palette';
-import { Text } from '../../atoms/typography';
-import { distanceBetweenDates } from '../../../lib/utils';
+import { FC } from "react";
+import { Palette } from "../../atoms/palette";
+import { Text } from "../../atoms/typography";
+import { distanceBetweenDates } from "../../../lib/utils";
+import { useTranslation } from "react-i18next";
 
 type TimeStatusProps = {
   endDate: string;
@@ -14,42 +15,44 @@ export const TimeStatus: FC<TimeStatusProps> = ({
   startDate,
   status,
 }) => {
+  const { t } = useTranslation();
   const renderTimeStatus = () => {
     const result = distanceBetweenDates(
       startDate || String(new Date()),
       endDate
     );
 
-    if (result < 7 && status !== 'completed') {
+    if (status === "completed") {
       return (
-        <Text size="body" weight="medium" colour={Palette.warning.W400}>
-          ⏳ Running out of time
-        </Text>
-      );
-    } else if (result > 7 && result < 30 && status !== 'completed') {
-      return (
-        <Text size="body" weight="medium" colour={Palette.warning.W400}>
-          💨 Time to step up
-        </Text>
-      );
-    } else if (result < 0 && status !== 'completed') {
-      return (
-        <Text size="body" weight="medium" colour={Palette.danger.D300}>
-          ⏳ Out of time
+        <Text size="body" weight="medium" colour={Palette.success.S300}>
+          {t("todo.time_status.completed")}
         </Text>
       );
     }
 
-    if (status === 'completed')
+    if (result < 7) {
       return (
-        <Text size="body" weight="medium" colour={Palette.success.S300}>
-          🎉 Well done
+        <Text size="body" weight="medium" colour={Palette.warning.W400}>
+          {t("todo.time_status.out_of_time")}
         </Text>
       );
+    } else if (result > 7 && result < 30) {
+      return (
+        <Text size="body" weight="medium" colour={Palette.warning.W400}>
+          {t("todo.time_status.step_up")}
+        </Text>
+      );
+    } else if (result < 0) {
+      return (
+        <Text size="body" weight="medium" colour={Palette.danger.D300}>
+          {t("todo.time_status.overdue")}
+        </Text>
+      );
+    }
 
     return (
       <Text size="body" weight="medium" colour={Palette.success.S300}>
-        ⏰ On time
+        {t("todo.time_status.on_time")}
       </Text>
     );
   };
