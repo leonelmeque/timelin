5ff42678d296712a855fd5bde0ea4485b3526f57
Leonel Meque <leomeque@gmail.com>
2023-05-30 08:35:00 +0200

    feat(ui): add label to badge

--- src/ui/atoms/badge/index.tsx
+++ src/ui/atoms/badge/index.tsx
@@ -1,28 +1,29 @@
-import { FC } from 'react';
-import { BadgeBGColor, BadgeTextColor } from '../../shared-types';
-import { Text } from '../typography';
-import { StyledBadge } from './styles';
-import { TodoStatus } from '../../../lib/shared-types';
-
+import { FC } from "react";
+import { BadgeBGColor, BadgeTextColor } from "../../shared-types";
+import { Text } from "../typography";
+import { StyledBadge } from "./styles";
+import { TodoStatus } from "../../../lib/shared-types";
 
 type BadgeProps = {
   status: TodoStatus;
-  type: 'colored' | 'simple';
+  label?: string;
+  type: "colored" | "simple";
 };
 
-export const Badge: FC<BadgeProps> = ({ status, ...rest }) => (
+export const Badge: FC<BadgeProps> = ({ status, label, ...rest }) => (
   <StyledBadge bg={BadgeBGColor[status]} {...rest}>
     <Text
       size="small"
       weight="medium"
-      colour={rest.type === 'simple' ? '' : BadgeTextColor[status]}
+      colour={rest.type === "simple" ? "" : BadgeTextColor[status]}
     >
-      {(
-        status.substring(0, 1).toUpperCase() +
-        status.substring(1, status.length).toLocaleLowerCase()
-      ).replace(/_/g, ' ')}
+      {label ||
+        (
+          status.substring(0, 1).toUpperCase() +
+          status.substring(1, status.length).toLocaleLowerCase()
+        ).replace(/_/g, " ")}
     </Text>
   </StyledBadge>
 );
 
-Badge.displayName = 'Badge';
+Badge.displayName = "Badge";
